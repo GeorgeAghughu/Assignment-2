8a8970788b2d4e0535f34669d05ec45e208be0c7
GBROWN <98670317+GeorgeAghughu@users.noreply.github.com>
2022-02-06 19:46:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let school;
+school = 'class';
+console.log(school);
+
+let building = 'room';
+console.log(building); 'building'
+
+let schoolbuilding = 'class' + 'room';
+console.log(schoolbuilding); 'classroom'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
